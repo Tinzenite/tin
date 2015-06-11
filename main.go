76e7f4caa4ea6dc4b5eaf7a518bf12c21d2abb4c
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-06-11 16:07:39 +0200

    adopted to Core

--- main.go
+++ main.go
@@ -2,7 +2,19 @@ package main
 
 import "github.com/tinzenite/core"
 
+// TODO check what Golang offers us to help here
+const configPath = ""
+
 func main() {
+	context, _ := loadContext()
 	// just run core for now
-	core.Run()
+	core.Run(*context)
+}
+
+// loadContext or create a new one if required. If new will save it.
+func loadContext() (*core.Context, error) {
+	// create new context to use
+	context, err := core.NewContext("NewTest")
+	// TODO check if one already exists, if yes use it. Otherwise create new one and save it.
+	return context, err
 }
