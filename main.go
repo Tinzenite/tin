52e63f92398e92a8885427dd0be60fb24230d466
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-06-28 17:44:36 +0200

    removed unneeded tests

--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"os/signal"
-	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/tinzenite/core"
 )
@@ -17,92 +14,7 @@ const path = "/home/tamino/Music"
 const user = "Xamino"
 
 func main() {
-	// model()
-	// walkTest()
-	// channel()
 	tinzenite()
-}
-
-func model() {
-	start := time.Now()
-	m, err := core.LoadModel(path)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	elapsed := time.Since(start)
-	elapsed = elapsed / time.Millisecond
-	// log.Printf("Output:\n\n%s\n", m.String())
-	log.Printf("Took %d msec\n", elapsed)
-	/*
-		obj, err := m.Read()
-		if err != nil {
-			log.Println(err.Error())
-			return
-		}
-		bin, _ := json.MarshalIndent(obj, "", "  ")
-		log.Println(string(bin))
-	*/
-	// register channel for updates
-	updates := make(chan core.UpdateMessage, 1)
-	m.Register(updates)
-	go func() {
-		for {
-			update := <-updates
-			log.Printf("Update received!\nType: %s\nContent: %+v\n", update.Operation, update.Object)
-		}
-	}()
-	// now allow manual operations
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		input, _ := reader.ReadString('\n')
-		if strings.Contains(input, "exit") {
-			break
-		}
-		start := time.Now()
-		err := m.Update()
-		elapsed := time.Since(start)
-		if err != nil {
-			log.Println(err.Error())
-		}
-		// log.Printf("Output:\n\n%s\n", m.String())
-		log.Printf("Updated in %s\n", elapsed)
-	}
-}
-
-type t struct {
-}
-
-func (*t) CallbackMessage(address, message string) {
-	log.Println("Incomming: " + message)
-}
-
-func (*t) CallbackNewConnection(address, message string) {
-	log.Println("Accepting: " + message)
-}
-
-func channel() {
-	channel, err := core.CreateChannel("test", nil, &t{})
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	address, _ := channel.Address()
-	log.Println("ID:\n" + address)
-	for {
-		select {
-		case <-c:
-			channel.Close()
-			goto exit
-		case <-time.Tick(10 * time.Second):
-			err := channel.Send("ed284a9fa07142cb8f6fa8c821d7f722cf63d2c7f74390566c6949bdb898b33e", "Tick!")
-			if err != nil {
-				log.Println(err.Error())
-			}
-		}
-	}
-exit:
 }
 
 func tinzenite() bool {
@@ -150,27 +62,5 @@ func tinzenite() bool {
 		}
 	}
 	tinzenite.Close()
-	return false
-}
-
-func walkTest() bool {
-	testpath := "/home/tamino/Programming"
-	ignorepath := "/home/tamino"
-	matcher, err := core.CreateMatcher(ignorepath)
-	if err != nil {
-		log.Println(err.Error())
-		return false
-	}
-	var objects int
-	var count int64
-	filepath.Walk(testpath, func(path string, info os.FileInfo, err error) error {
-		if matcher.Ignore(path) {
-			objects++
-			return filepath.SkipDir
-		}
-		count += info.Size()
-		return nil
-	})
-	log.Printf("Kept %dkb worth of objects, ignored %d objects\n", count/1024, objects)
 	return false
 }
