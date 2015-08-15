b5ec97204333c8c8b91e6045fbd3ffb847be1885
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-08-15 14:16:54 +0200

    better question

--- main.go
+++ main.go
@@ -15,17 +15,20 @@ func main() {
 	// read them
 	flag.Parse()
 	// ask which operation to do
-	opQuestion := question{question: "(L)oad a Tinzenite directory, (C)reate one, or (B)ootstrap to an existing one?"}
-	opQuestion.createAnswer(0, "L", "l", "Load", "load")
-	opQuestion.createAnswer(1, "C", "c", "Create", "create")
-	opQuestion.createAnswer(2, "B", "b", "Bootstrap", "bootstrap")
+	opQuestion := createQuestion("(L)oad a Tinzenite directory, (C)reate one, or (B)ootstrap to an existing one?")
+	opQuestion.createAnswer(0, "l", "load")
+	opQuestion.createAnswer(1, "c", "create")
+	opQuestion.createAnswer(2, "b", "bootstrap")
 	switch opQuestion.ask() {
 	case 0:
 		/*TODO load*/
+		log.Println("Load")
 	case 1:
 		/*TODO create*/
+		log.Println("Create")
 	case 2:
 		/*TODO bootstrap*/
+		log.Println("Bootstrap")
 	default:
 		log.Println("Question returned unknown operation!")
 		return
